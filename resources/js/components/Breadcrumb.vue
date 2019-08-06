451fba1632c3206a9b05895389e32a50214433bd
Yohan Apriyandi <cepoangkuningan@gmail.com>
2019-08-06 16:39:43 +0700

    add breadcrum on components directory

---
+++ resources/js/components/Breadcrumb.vue
@@ -0,0 +1,11 @@
+<template>
+    <ol class="breadcrumb">
+        <li><router-link :to="{ name:'home' }" ><i class="fa fa-dashboard"> Home</i> </router-link></li>
+        <li class="active">{{ $route.meta.title }}</li>
+    </ol>
+</template>
+<script>
+export default {
+
+}
+</script>
