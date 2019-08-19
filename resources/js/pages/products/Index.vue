2a484852dd10add5eb31a9bfc3cdb9af3ba148cd
Yohan Apriyandi <cepoangkuningan@gmail.com>
2019-08-19 13:44:06 +0700

    add view for  create, update and delete products

---
+++ resources/js/pages/products/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="content-wrapper">
+        <div class="container">
+            <section class="content-header">
+                <h1>
+                    Manage Products
+                </h1>
+                <breadcrumb></breadcrumb>
+            </section>
+
+            <section class="content">
+                <div class="row">
+                    <router-view></router-view>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+<script>
+import Breadcrumb from '../../components/Breadcrumb.vue'
+export default {
+    name: 'Indexproduct',
+    components: {
+        'breadcrumb': Breadcrumb
+    }
+}
+</script>
