edf168accf9af28ef7aaaad0356280519f42eeaa
Yohan89 <cepoangkuningan@gmail.com>
2019-09-21 19:26:17 +0700

    update file

--- app/Http/Controllers/API/RolePermissionController.php
+++ app/Http/Controllers/API/RolePermissionController.php
@@ -19,38 +19,38 @@ class RolePermissionController extends Controller
 
     public function getAllPermission()
     {
-        $permission = Permission::all();
+        $permission = Permission::all();        
         return response()->json(['status' => 'success', 'data' => $permission], 200);
     }
 
     public function getRolePermission(Request $request)
-    {
+    {   
         $hasPermission = DB::table('role_has_permissions')
             ->select('permissions.name')
-            ->join('permissions', 'role_has_permissions.permission_id', '=', 'permissions.id' )
+            ->join('permissions', 'role_has_permissions.permission_id', '=', 'permissions.id')
             ->where('role_id', $request->role_id)->get();
-            return response()->json(['status' => 'success', 'data' => $hasPermission], 200);
+            return response()->json(['status' => 'success', 'data' => $hasPermission]);
     }
 
     public function setRolePermission(Request $request)
     {
         $this->validate($request, [
-            'role_id' => 'required|exists:roles. id'
+            'role_id' => 'required|exists:roles,id'
         ]);
         $role = Role::find($request->role_id);
-        $role->syncPermission($request->permission);
-        return response()->json(['status' => 'success'], 200);
+        $role->syncPermissions($request->permissions);     
+        return response()->json(['status' => 'success']);
     }
 
     public function  setRoleUser(Request $request)
     {
-        $this->validate([
+        $this->validate($request,[
             'user_id' => 'required|exists:users, id',
             'role' => 'required'
         ]);
         $user = User::find($request->user_id);
         $user->syncRoles([$request->role]);
-        return response()->json(['status' => 'success'], 200);
+        return response()->json(['status' => 'success']);
     }
 
 }
