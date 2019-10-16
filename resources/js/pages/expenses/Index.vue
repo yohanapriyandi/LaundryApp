e225cbdc724f0ed92e8ce2e6b14c1ba2ceb49ea7
Yohan89 <cepoangkuningan@gmail.com>
2019-10-16 20:28:23 +0700

    pusher notification laravel-vuejs

---
+++ resources/js/pages/expenses/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="content-wrapper">
+        <div class="container">
+            <section class="content-header">
+                <h1>
+                    Manage Expenses
+                </h1>
+                <breadcrumb></breadcrumb>
+            </section>
+            <section class="content">
+                <div class="row">
+                    <router-view></router-view>
+                </div>
+            </section>
+        </div>
+    </div>
+
+</template>
+<script>
+import Breadcrumb from '../../components/Breadcrumb.vue'
+export default {
+    name:'IndexExpenses',
+    components: {
+        'breadcrumb': Breadcrumb
+    }
+}
+</script>
