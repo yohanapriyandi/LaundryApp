744e9cf8f1fb908376dfe77a1b4f2ba75a2002ba
Yohan89 <cepoangkuningan@gmail.com>
2019-10-25 10:04:08 +0700

    add relation to other table

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $guarded = [];
+    protected $appends = ['status_label'];
+
+    public function detail()
+    {
+        return $this->hasMany(DetailTransaction::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getStatusLabelAttribute()
+    {
+        if ($this->status == 1) {
+            return '<span class="label label-success"> Selesai</span>';
+        }
+        return '<span class="label label-primary">Proses</span>';
+    }
 }
