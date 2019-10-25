ab968c7297c974fcc96f5bd24a50be6a110c6abb
Yohan89 <cepoangkuningan@gmail.com>
2019-10-25 09:51:47 +0700

    pages for transaction

---
+++ resources/js/pages/transactions/Add.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="col-md-12">
+        <div class="panel">
+            <div class="panel-heading">
+                <h3 class="panel-title">Create New Transaction</h3>
+            </div>
+
+            <div class="panel-body">
+                <transaction-form ref="form"></transaction-form>
+                <div class="form-group">
+                    <button class="btn btn-microsoft btn-sm btn-flat" @click.prevent="submit">
+                        <i class="fa fa-save"> Create Transaction</i>
+                    </button>
+                    <!-- tombol dengan fungsi untuk mengosongkan form  -->
+                     <button class="btn btn-microsoft btn-sm btn-flat" @click.prevent="clearForm">
+                        <i class="fa fa-save"> Clear Form</i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapActions, mapState, mapMutations } from 'vuex'
+import FormTransaction from './Form.vue'
+export default {
+    name: 'AddTransaction',
+
+    methods: {
+        submit() {
+            this.$refs.form.submit()
+        },
+        clearForm() {
+            this.$refs.form.resetForm()
+        }
+    },
+
+    components: {
+         'transaction-form': FormTransaction
+    }
+}
+</script>
---
+++ resources/js/pages/transactions/Index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="content-wrapper">
+        <div class="container">
+            <section class="content-header">
+                <h1>
+                    Manage Transaction
+                </h1>
+                <breadcrumb></breadcrumb>
+            </section>
+            <section class="content">
+                <div class="row">
+                    <router-view></router-view>
+                </div>
+            </section>
+        </div>
+    </div>
+
+</template>
+<script>
+import Breadcrumb from '../../components/Breadcrumb.vue'
+export default {
+    name:'IndexTransaction',
+    components: {
+        'breadcrumb': Breadcrumb
+    }
+}
+</script>
